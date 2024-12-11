196fe81886691c61eb4b05a5fa51f48197207c96
Ri4yy <maksimov.1913@list.ru>
2024-12-11 12:51:57 +0300

    add header&footer&banner

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,26 +1,36 @@
 document.addEventListener('DOMContentLoaded', () => {
-    let menu = document.querySelector('.menu'),
-    btnMenu = document.querySelector('.btn-menu');
+    // let menu = document.querySelector('.menu'),
+    // btnMenu = document.querySelector('.btn-menu');
 
-    btnMenu.addEventListener('click', (e) => {
-        menu.classList.toggle('menu--open')
+    // btnMenu.addEventListener('click', (e) => {
+    //     menu.classList.toggle('menu--open')
 
-        btnMenu.classList.toggle('btn-menu--open')
-    })
+    //     btnMenu.classList.toggle('btn-menu--open')
+    // })
 
-    function resize() {
-        let width = window.innerWidth;
+    // function resize() {
+    //     let width = window.innerWidth;
 
-        if (width > 768) {
-            menu.classList.remove('menu--open')
-            btnMenu.classList.remove('btn-menu--open')
-        } else {
-            return
-        }
+    //     if (width > 768) {
+    //         menu.classList.remove('menu--open')
+    //         btnMenu.classList.remove('btn-menu--open')
+    //     } else {
+    //         return
+    //     }
+    // }
+
+    // window.addEventListener('resize', () => {
+    //     resize()
+    // })
+    // resize()
+
+
+    function tabs(wrapperMain, wrapperTab, wrapperContent, activeTab, activeContent) {
+        $(wrapperTab).on('click', 'li:not('+activeTab+')', function () {
+            $(this)
+                .addClass(activeTab).siblings().removeClass(activeTab)
+                .closest(wrapperMain).find(wrapperContent).removeClass(activeContent).eq($(this).index()).addClass(activeContent);
+        });
     }
-
-    window.addEventListener('resize', () => {
-        resize()
-    })
-    resize()
+    tabs('.tabs', '.tabs__list', '.tabs__content', 'active-tab', 'active');
 })
--- assets/js/swiper.js
+++ assets/js/swiper.js
@@ -1,72 +1,103 @@
-const swiper = new Swiper('.swiper', {
-    // Optional parameters
-    spaceBetween: 20,
-    enabled: true,
+// const swiper = new Swiper('.swiper', {
+//     // Optional parameters
+//     spaceBetween: 20,
+//     enabled: true,
 
-    breakpoints: {
-        320: {
-            allowTouchMove: true,
-            slidesPerView: 1,
-            grid: {
-                rows: 1,
-                fill: "row",
-            }
-        },
-        480: {
-            allowTouchMove: true,
-            slidesPerView: 2,
-            grid: {
-                rows: 1,
-                fill: "row",
-            }
-        },
-        768: {
-            allowTouchMove: true,
-            slidesPerView: 3,
-            grid: {
-                rows: 1,
-                fill: "row",
-            }
-        },
-        1024: {
-            allowTouchMove: true,
-            slidesPerView: 2,
-            grid: {
-                rows: 2,
-                fill: "row",
-            }
-        },
-        1280: {
-            allowTouchMove: true,
-            slidesPerView: 3,
-            grid: {
-                rows: 2,
-                fill: "row",
-            }
-        },
-        1921: {
-            allowTouchMove: true,
-            slidesPerView: 4,
-            grid: {
-                rows: 2,
-                fill: "row",
-            }
-        },
-    },
+//     breakpoints: {
+//         320: {
+//             allowTouchMove: true,
+//             slidesPerView: 1,
+//             grid: {
+//                 rows: 1,
+//                 fill: "row",
+//             }
+//         },
+//         480: {
+//             allowTouchMove: true,
+//             slidesPerView: 2,
+//             grid: {
+//                 rows: 1,
+//                 fill: "row",
+//             }
+//         },
+//         768: {
+//             allowTouchMove: true,
+//             slidesPerView: 3,
+//             grid: {
+//                 rows: 1,
+//                 fill: "row",
+//             }
+//         },
+//         1024: {
+//             allowTouchMove: true,
+//             slidesPerView: 2,
+//             grid: {
+//                 rows: 2,
+//                 fill: "row",
+//             }
+//         },
+//         1280: {
+//             allowTouchMove: true,
+//             slidesPerView: 3,
+//             grid: {
+//                 rows: 2,
+//                 fill: "row",
+//             }
+//         },
+//         1921: {
+//             allowTouchMove: true,
+//             slidesPerView: 4,
+//             grid: {
+//                 rows: 2,
+//                 fill: "row",
+//             }
+//         },
+//     },
   
-    // If we need pagination
-    pagination: {
-      el: '.swiper-pagination',
-    },
+//     // If we need pagination
+//     pagination: {
+//       el: '.swiper-pagination',
+//     },
   
-    // Navigation arrows
-    navigation: {
-      nextEl: '.swiper-button-next',
-      prevEl: '.swiper-button-prev',
-    },
+//     // Navigation arrows
+//     navigation: {
+//       nextEl: '.swiper-button-next',
+//       prevEl: '.swiper-button-prev',
+//     },
   
-    // And if we need scrollbar
-    scrollbar: {
-      el: '.swiper-scrollbar',
-    },
-});
+//     // And if we need scrollbar
+//     scrollbar: {
+//       el: '.swiper-scrollbar',
+//     },
+// });
+
+document.addEventListener('DOMContentLoaded', () => {
+    const swiperBanner = new Swiper('.swiper-banner', {
+        // Optional parameters
+        slidesPerView: 1,
+        enabled: true,
+        speed: 1200,
+        parallax: true,
+        grabCursor: true,
+      
+        // If we need pagination
+        pagination: {
+            el: ".banner__counter",
+            type: "custom",
+
+            renderCustom: function (swiper, current, total) {
+                return `<span class="banner__counter-current">
+                            ${current < 10 ? '0' + current : current}
+                        </span> 
+                        <span class="banner__counter-total">
+                            /${total < 10 ? '0' + total : total}
+                        </span>`;
+              }
+        },
+      
+        navigation: {
+            nextEl: '.banner__bottom-next',
+            prevEl: '.banner__bottom-prev',
+        },
+    });
+})
